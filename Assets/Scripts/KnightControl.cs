754d57048c5033a0ea1325fec818fec957b8f92c
WeiEnZero <shutman89@gmail.com>
2025-07-09 11:10:30 +0800

    Added New 2D Animation to the Character
    Managed to make the character have the idle animation on the idle position and make the character walk with animation from input using animator component

--- Assets/Scripts/KnightControl.cs
+++ Assets/Scripts/KnightControl.cs
@@ -8,11 +8,13 @@ public class KnightControl : MonoBehaviour
     [SerializeField] private float speed = 4.0f;
     float speedX, speedY;
     private Rigidbody2D rigidBody;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
@@ -22,6 +24,34 @@ public class KnightControl : MonoBehaviour
         speedX = Input.GetAxisRaw("Horizontal") * speed;
         speedY = Input.GetAxisRaw("Vertical") * speed;
         rigidBody.linearVelocity = new Vector2(speedX, speedY);
+
+        if (speedX > 0)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        } else if (speedX < 0) {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 
+    private void FixedUpdate()
+    {
+        if (rigidBody.linearVelocity.magnitude > 0)
+        {
+            WalkAnimation();
+        }
+        else
+        {
+            IdleAnimation();
+        }
+    }
+
+    public void WalkAnimation()
+    {
+        animator.Play("Walk");
+    }
+
+    public void IdleAnimation()
+    {
+        animator.Play("Idle");
+    }
 }
