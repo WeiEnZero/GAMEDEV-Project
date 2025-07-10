d8b16781dcc03c64c364f0149b411daa8708f645
WeiEnZero <shutman89@gmail.com>
2025-07-10 11:09:48 +0800

    Added Tilemaps to Game World
    Used Tile Pallete and free assests to add Tilemaps to the game's ground and changed the Z position to make the character walk on the tiles.

---
+++ Assets/Scripts/HeroControl.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroControl : MonoBehaviour
+{
+    [SerializeField] private float speed = 4.0f;
+    float speedX, speedY;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * speed;
+        speedY = Input.GetAxisRaw("Vertical") * speed;
+        rigidBody.linearVelocity = new Vector2(speedX, speedY);
+
+        if (speedX > 0)
+        {
+            gameObject.transform.localScale = new Vector3(2 , 2 , 1);
+        } 
+        else if (speedX < 0) 
+        {
+            gameObject.transform.localScale = new Vector3(-2, 2, 1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rigidBody.linearVelocity.magnitude > 0)
+        {
+            WalkAnimation();
+        }
+        else
+        {
+            IdleAnimation();
+        }
+    }
+
+    public void WalkAnimation()
+    {
+        animator.Play("Walk");
+    }
+
+    public void IdleAnimation()
+    {
+        animator.Play("Idle");
+    }
+}
