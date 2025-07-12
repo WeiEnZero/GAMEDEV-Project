9296d105fa02d0b28005a7a944974767f12c64c6
WeiEnZero <shutman89@gmail.com>
2025-07-12 16:37:24 +0800

    Finished Setting Game World and Set collision detection
    Finished setting up game world and set colliders to obstacles, character and outside world boundaries.

--- Assets/Scripts/HeroControl.cs
+++ Assets/Scripts/HeroControl.cs
@@ -21,22 +21,25 @@ public class HeroControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //The input would get the raw axis of X and Y accoriding to vertical and horizontal direction
         speedX = Input.GetAxisRaw("Horizontal") * speed;
         speedY = Input.GetAxisRaw("Vertical") * speed;
+        // The X and Y values are then used as arguments for Vector2 when the RigidBody.Velocity is used for character control on the X and Y axis
         rigidBody.linearVelocity = new Vector2(speedX, speedY);
 
         if (speedX > 0)
         {
-            gameObject.transform.localScale = new Vector3(2 , 2 , 1);
+            gameObject.transform.localScale = new Vector3(4.7726f, 4.7726f, 1);
         } 
         else if (speedX < 0) 
         {
-            gameObject.transform.localScale = new Vector3(-2, 2, 1);
+            gameObject.transform.localScale = new Vector3(-4.7726f, 4.7726f, 1);
         }
     }
 
     private void FixedUpdate()
     {
+        // If the movement magnitude of the hero is more than 0, the walk animation function is used else the Idle Animation is active.
         if (rigidBody.linearVelocity.magnitude > 0)
         {
             WalkAnimation();
@@ -47,6 +50,7 @@ public class HeroControl : MonoBehaviour
         }
     }
 
+    //Functions to get the specific animations from the Animator Component.
     public void WalkAnimation()
     {
         animator.Play("Walk");
