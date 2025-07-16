d9f474b82bb33135783bd6201cd0c324da1dc6e4
WeiEnZero <shutman89@gmail.com>
2025-07-16 15:59:53 +0800

    Added Enemy Sprite to Chase Player
    Added Enemy Sprite to Chase Player using a simple AI code and some Vector 2 calculations.

---
+++ Assets/Scripts/EnemyAIChase.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EnemyAIChase : MonoBehaviour
+{
+    public Transform Player;
+    public float speed;
+    public float distanceBetween;
+    private float distance;
+    float Xaxis;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player = GameObject.Find("Hero").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Xaxis = Input.GetAxisRaw("Horizontal") * speed;
+        distance = Vector2.Distance(transform.position, Player.transform.position);
+        Vector2 direction = Player.transform.position - transform.position;
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
+
+
+        if (Xaxis > 0)
+        {
+            gameObject.transform.localScale = new Vector3(1.499631f, 1.499631f, 1);
+        }
+        else if (Xaxis < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-1.499631f, 1.499631f, 1);
+        }
+
+    }
+}
